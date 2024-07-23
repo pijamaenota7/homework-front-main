79290bce9914e6073101ce3c498e0b902a6ad24a
pijamaenota7 <zxcodingninja@gmail.com>
2024-07-23 12:33:27 +0200

    hw1

---
+++ src/s1-main/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import s from './App.module.css'
+import HW1 from '../s2-homeworks/hw01/HW1'
+
+function App() {
+    return (
+        <div className={s.App}>
+            <HW1 />
+        </div>
+    )
+}
+
+export default App
